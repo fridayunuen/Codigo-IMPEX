b2f4a49a7fde53824e08d3b07dcce9c062ca3017
Frida Colín <fridayunuen99@gmail.com>
2022-08-12 17:03:29 -0500

    Consideracion No acceso VPN

--- ReporteCompartido.R
+++ ReporteCompartido.R
@@ -5,12 +5,47 @@ Fecha<-as.character.Date(Sys.time())
 repgenerado<-data.frame(itemsgenerados, Responsable,Fecha)
 
 
-setwd("S:/OMNI/ReporteGeneral")
-rep<-read.csv("ProductosIMPEX.csv")
-rep<-data.frame(rep)
 
-rep<-rbind(rep, repgenerado)
+if(dir.exists("S:/OMNI/ReporteGeneral")){
+  
+  setwd("S:/OMNI/ReporteGeneral")
+  
+  rep<-read.csv("ProductosIMPEX.csv")
+  rep<-data.frame(rep)
+  
+  rep<-rbind(rep, repgenerado)
+  
+  write.csv(rep, "ProductosIMPEX.csv", row.names = FALSE)
+  
+  
+  
+}else{
+  setwd(guardar)
+  
+  if (file.exists("ENVIAR-a-RESPONSABLE-ProductosIMPEX.csv")){
+    
+    rep<-read.csv("ENVIAR-a-RESPONSABLE-ProductosIMPEX.csv")
+    rep<-data.frame(rep)
+    
+    rep<-rbind(rep, repgenerado)
+    
+    write.csv(rep, "ENVIAR-a-RESPONSABLE-ProductosIMPEX.csv", row.names = FALSE)
+    
+    
+  }else{
+    write.csv(repgenerado, "ENVIAR-a-RESPONSABLE-ProductosIMPEX.csv", row.names = FALSE)
+    
+    
+  }
+  
+  
+}
 
-write.csv(rep, "ProductosIMPEX.csv", row.names = FALSE)
+
 
 setwd(DirectorioPadre)
+
+
+
+
+
--- nosku.R
+++ nosku.R
@@ -2,10 +2,6 @@
 if(!is.null(reporte)){
   if(unique(reporte$Estado) == "El SKU no se encuentra en cubo"){
     
-    setwd("S:/OMNI/ReporteGeneral")
-    
-    csv<-read.csv("ProblemasSKU.csv")
-    
     Producto<-reporte$Archivo
     
     Responsable <- as.character(Sys.info()["user"])
@@ -14,9 +10,42 @@ if(!is.null(reporte)){
     
     nosku<-data.frame("NoSKUCubo", Producto, Responsable, Fecha)
     
-    nosku<-rbind(csv, nosku)
+    if(dir.exists("S:/OMNI/ReporteGeneral")){
+      setwd("S:/OMNI/ReporteGeneral")
+      
+      csv<-read.csv("ProblemasSKU.csv")
+      
+      nosku<-rbind(csv, nosku)
+      
+      write.csv(nosku, "ProblemasSKU.csv", row.names = FALSE)
+      
+    }else{
+      
+      setwd(guardar)
+      
+      if (file.exists("ENVIAR-a-RESPONSABLE-ProblemasSKU.csv")){
+        
+        
+        csv<-read.csv("ENVIAR-a-RESPONSABLE-ProblemasSKU.csv")
+        csv<-data.frame(csv)
+        
+        csv<-rbind(csv, nosku)
+        
+        write.csv(csv, "ENVIAR-a-RESPONSABLE-ProblemasSKU.csv", row.names = FALSE)
+        
+        
+      }else{
+        write.csv(nosku, "ENVIAR-a-RESPONSABLE-ProblemasSKU.csv", row.names = FALSE)
+        
+        
+      }
+      
+      
+      
+      
+    }
     
-    write.csv(nosku, "ProblemasSKU.csv", row.names = FALSE)
+    
     
   }
   
